346251ca4eb8ef8d3299dee29984ed4bbed9e96d
linhdo39 <linh.do9397@gmail.com>
2022-05-04 18:54:22 -0500

    Update seed.py

--- app/recipes/management/commands/seed.py
+++ app/recipes/management/commands/seed.py
@@ -1,11 +1,15 @@
 import requests
 from django.core.management.base import BaseCommand
 from recipes.models import Recipe
+from recipes.info import APP_ID
+from recipes.info import APP_KEY
 
+#remember to install python requests
 def get_data():
+    id = APP_ID
+    key = APP_KEY
     #setup the http request
-    url = 'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=dinner&app_id=5e46e2fc&app_key=73242337544cac1a326435a7ab5bec29&random=true'
-
+    url = 'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=all&app_id=' + id+ '&app_key='+key
     r = requests.get(url, headers={'Content-Type':      
     'application/json'})
     #save the json respone into variable
@@ -14,9 +18,18 @@ def get_data():
     return recipe['hits']
 
 def seed_data():
-    for item in get_data():
-        recipe = Recipe(
-            name = item["recipe"]["label"]
+    items = get_data()
+    for item in items:
+        recipe = Recipe (
+            name = item["recipe"]["label"],
+            recipe_url = item["recipe"]["uri"],
+            image_url = item["recipe"]["image"],
+            ingredient = item["recipe"]["ingredientLines"],
+            calories = item["recipe"]["calories"],
+            total_time = item["recipe"]["totalTime"],
+            cuisine = item["recipe"]["cuisineType"],
+            mealType = item["recipe"]["mealType"],
+            dishType = item["recipe"]["dishType"]
         )
 
         recipe.save()
