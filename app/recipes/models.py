5607e3e494a4964b00cd194e59273dce3ca0e28c
kylem164 <kylemastrangeli@gmail.com>
2022-05-04 20:22:18 -0500

    initial database design complete

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -5,5 +5,8 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    recipe_id = models.CharField(max_length=500)
-    
+    recipe_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
