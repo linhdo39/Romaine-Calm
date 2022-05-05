aaf86a7ef64e8966fac6cb94dd48aea1263821fb
linhdo39 <linh.do9397@gmail.com>
2022-05-04 19:57:24 -0500

    Update models.py

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -5,11 +5,5 @@ from django.db import models
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    recipe_url = models.CharField(max_length=500)
-    image_url = models.CharField(max_length=500)
-    ingredient = models.CharField(max_length=500)
-    calories = models.CharField(max_length=50)
-    total_time = models.CharField(max_length=10)
-    cuisine = models.CharField(max_length=100)
-    mealType = models.CharField(max_length=100)
-    dishType = models.CharField(max_length=100)
+    recipe_id = models.CharField(max_length=500)
+    
