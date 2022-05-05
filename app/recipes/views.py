2fd27f6da95b75262c04bda18e0c2b05621fc16f
kylem164 <kylemastrangeli@gmail.com>
2022-05-05 18:39:47 -0500

    template changes with databse query example

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Recipe
 
 # Create your views here.
 
 def index_view(response):
     #return HttpResponse("recipe home")
-    return render(response, "recipes/home.html", {})
+    recipe_list = Recipe.objects.all()
+    return render(response, "recipes/home.html", {'recipe_list': recipe_list})
