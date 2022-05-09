f8115d7712d43dd92351d8f24af55ff7a383093b
linhdo39 <linh.do9397@gmail.com>
2022-05-08 23:20:26 -0500

    Update seed.py

--- app/recipes/management/commands/seed.py
+++ app/recipes/management/commands/seed.py
@@ -5,29 +5,34 @@ from recipes.info import APP_ID
 from recipes.info import APP_KEY
 
 #remember to install python requests
-def get_data():
-    id = APP_ID
-    key = APP_KEY
+def get_data(url):
+   
     #setup the http request
-    url = 'https://api.edamam.com/api/recipes/v2?type=public&beta=true&q=all&app_id=' + id+ '&app_key='+key
     r = requests.get(url, headers={'Content-Type':      
     'application/json'})
     #save the json respone into variable
     recipe = r.json()
     #return the json
-    return recipe['hits']
+    return recipe
 
 def seed_data():
-    items = get_data()
-    for item in items:
-        uri = item["recipe"]["uri"]
-        uri = uri.replace("http://www.edamam.com/ontologies/edamam.owl#recipe_","")
-        recipe = Recipe (
-            name = item["recipe"]["label"],
-            recipe_id = uri         
-        )
+    categories = {"breakfast", "brunch", "lunch", "dinner", "dessert"}
+    for category in categories:
+        url = "https://api.edamam.com/api/recipes/v2?type=public&beta=true&q="+ category +"&app_id=" + APP_ID+ "&app_key="+APP_KEY
+        items = get_data(url)
+        for i in range (0, 10,1):
+            if(items["hits"] is None):
+                break
+            for item in items["hits"]:
+                uri = item["recipe"]["uri"]
+                uri = uri.replace("http://www.edamam.com/ontologies/edamam.owl#recipe_","")
+                recipe = Recipe (
+                    name = item["recipe"]["label"],
+                    recipe_id = uri         
+                )
 
-        recipe.save()
+                recipe.save()
+            items = get_data(items["_links"]["next"]["href"])
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
