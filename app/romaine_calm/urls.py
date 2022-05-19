775dc4d508b6ca2b029fb16775c7b24237ee57e9
kylem164 <kylemastrangeli@gmail.com>
2022-05-19 17:43:09 -0400

    all routes working to soon.html template

--- app/romaine_calm/urls.py
+++ app/romaine_calm/urls.py
@@ -21,6 +21,9 @@ from pages.views import about_view
 from pages.views import homepage_view
 from pages.views import help_view
 from pages.views import profile_view
+from pages.views import news_view
+from pages.views import contact_view
+from pages.views import add_recipe_view
 from register import views as v
 from recipes.views import index_view
 from recipes.views import all_view
@@ -35,5 +38,10 @@ urlpatterns = [
     path('recipes/<id>', index_view),
     path('', include('django.contrib.auth.urls')),
     path('profile/', profile_view, name= 'profile'),
-    path('about/', about_view, name= 'about')
+    path('about/', about_view, name= 'about'),
+    path('news/', news_view, name= 'news'),
+    path('contact/', contact_view, name= 'contact'),
+    path('add_recipe/', add_recipe_view, name= 'add_recipe')
+
+
 ]
