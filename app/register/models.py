2741b712f8109ff7355220b97805e427a2ca30ec
elminabrkic <elminabrkic@icloud.com>
2022-05-19 19:39:59 -0500

    resoved conflicts

--- app/register/models.py
+++ app/register/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Meta:
     model = User
     fields = ["username", "email", "password1", "password2"]
     
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    def __str__(self):
+        return str(self.user)
+
--- app/register/views.py
+++ app/register/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from django.shortcuts import  get_object_or_404, render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import DetailView
+from django.views.generic import DetailView
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import UserChangeForm
+from .models import Profile
+from .forms import RegisterForm, EditProfileForm,PasswordChangingForm
+
 
 # Create your views here.
 def register(response):
@@ -11,3 +19,36 @@ def register(response):
     else:
         form = RegisterForm()
     return render(response, "register/register.html", {"form":form})
+
+
+class PasswordChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    success_url = reverse_lazy("edit_profile")
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = "registration/edit_profile.html"
+    success_url = reverse_lazy("home")
+
+    def get_object(self):
+        return self.request.user
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name ="registration/user_profile.html"
+
+    def get_context_data(self,*args, **kwargs):
+      
+        context = super(ShowProfilePageView,self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+        context['page_user'] = page_user
+        return context
+
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name ="registration/edit_profile_page.html"
+    success_url = reverse_lazy("home")
+    fields = ["bio","profile_image"]
+
+
