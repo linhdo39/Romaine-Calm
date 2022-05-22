0dcd8773e584c202919ac48dd77d0084314b0f23
elminabrkic <elminabrkic@icloud.com>
2022-05-22 15:19:50 -0500

    resolved conflicts

--- app/romaine_calm/settings.py
+++ app/romaine_calm/settings.py
@@ -141,9 +141,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
 LOGIN_REDIRECT_URL = "/recipes"
-<<<<<<< HEAD
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = 'login'
-=======
 LOGOUT_REDIRECT_URL = "/"
->>>>>>> parent of 2741b71 (resoved conflicts)
