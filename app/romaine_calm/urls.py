74db4d810f88975e79656afff56082c660e0cdd8
elminabrkic <elminabrkic@icloud.com>
2022-05-21 21:31:08 -0500

    fixing branch errors

--- app/romaine_calm/urls.py
+++ app/romaine_calm/urls.py
@@ -20,6 +20,10 @@ from django.urls import path
 from pages.views import about_view
 from pages.views import homepage_view
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+from pages.views import profile_view
+>>>>>>> 3909872 (adding new code)
 from pages.views import help_view
 from pages.views import profile_view
 =======
@@ -29,6 +33,9 @@ from register import views as v
 from recipes.views import index_view
 from recipes.views import all_view
 from register.views import UserEditView, PasswordChangeView,EditProfilePageView
+<<<<<<< HEAD
+>>>>>>> 3909872 (adding new code)
+=======
 >>>>>>> 3909872 (adding new code)
 from pages.views import news_view
 from pages.views import contact_view
@@ -38,6 +45,12 @@ from register import views as v
 from recipes.views import index_view
 from recipes.views import all_view
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+from register import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
+>>>>>>> 3909872 (adding new code)
 =======
 from register import views as user_views
 from django.conf import settings
@@ -53,12 +66,18 @@ urlpatterns = [
     path('recipes/<id>', index_view),
     path('', include('django.contrib.auth.urls')),
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('profile/', profile_view, name= 'profile'),
 =======
+=======
+>>>>>>> 3909872 (adding new code)
     path('profile/', user_views.profile, name="profile"),
     path('about/', about_view, name='about'),
     path("edit_profile/", UserEditView.as_view(), name="edit_profile"),
     path('password/',PasswordChangeView.as_view(template_name="registration/change-password.html")),
+<<<<<<< HEAD
+>>>>>>> 3909872 (adding new code)
+=======
 >>>>>>> 3909872 (adding new code)
     path('about/', about_view, name= 'about'),
     path('news/', news_view, name= 'news'),
@@ -66,7 +85,10 @@ urlpatterns = [
     path('add_recipe/', add_recipe_view, name= 'add_recipe'),
     path('favorite/', favorite_view, name= 'favorite')
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3909872 (adding new code)
 =======
 >>>>>>> 3909872 (adding new code)
 ]
