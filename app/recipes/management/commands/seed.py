84c8965c1d31ec215d7f0bab91eef244781b4c32
linhdo39 <linh.do9397@gmail.com>
2022-05-23 03:22:49 -0500

    Update seed.py

--- app/recipes/management/commands/seed.py
+++ app/recipes/management/commands/seed.py
@@ -16,19 +16,18 @@ def get_data(url):
     return recipe
 
 def seed_data():
-    categories = {"breakfast", "brunch", "lunch", "dinner", "dessert"}
+    categories = ["breakfast", "dinner", "dessert", "lunch","brunch"]
     for category in categories:
-        url = "https://api.edamam.com/api/recipes/v2?type=public&beta=true&q="+ category +"&app_id=" + APP_ID+ "&app_key="+APP_KEY
+        url = "https://api.edamam.com/api/recipes/v2?type=public&q="+category+"&app_id=" + APP_ID+ "&app_key="+APP_KEY
         items = get_data(url)
-        for i in range (0, 10,1):
-            if(items["hits"] is None):
-                break
+        for i in range (0, 10):
             for item in items["hits"]:
                 uri = item["recipe"]["uri"]
                 uri = uri.replace("http://www.edamam.com/ontologies/edamam.owl#recipe_","")
                 recipe = Recipe (
                     name = item["recipe"]["label"],
-                    recipe_id = uri         
+                    recipe_id = uri,
+                    category = category       
                 )
 
                 recipe.save()
