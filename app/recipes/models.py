89f406336d0b15632a18a94b065be5d6ef4112a5
linhdo39 <linh.do9397@gmail.com>
2022-05-23 03:22:53 -0500

    Update models.py

--- app/recipes/models.py
+++ app/recipes/models.py
@@ -1,4 +1,5 @@
 #from asyncio.windows_events import NULL
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
@@ -6,7 +7,7 @@ from django.contrib.auth.models import User
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     recipe_id = models.CharField(max_length=100)
-
+    category = models.CharField(max_length=100)
     def __str__(self):
         return self.name
 
@@ -14,3 +15,7 @@ class FavoriteRecipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
+class Ingredients(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient_name = models.CharField(max_length=100)
+    ingredient_quantity =  models.CharField(max_length=100)
